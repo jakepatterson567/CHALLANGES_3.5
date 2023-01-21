553a4406016dcf93da240e66f88382905b2e5f99
Jake <jakepatterson567@gmail.com>
2023-01-21 13:45:58 -0800

    :Pencil:test:PositiveNegativeZero

---
+++ src/PositiveNegativeZero.java
@@ -0,0 +1,29 @@
+public class PositiveNegativeZero {
+
+public static void checkNumber(int number) {
+
+
+
+        if (number > 0) {
+        System.out.println("positive");
+        } else if (number < 0) {
+        System.out.println("Negative");
+        } else if (number == 0) {
+        System.out.println("zero");
+        }
+
+
+        }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
